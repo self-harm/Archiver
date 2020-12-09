1a063dba169715a53f6c803e73c2d192e049d9c2
self-harm <lightgraywrite@gmail.com>
2020-12-10 00:42:13 +0300

    Create ZipFileManager.java

---
+++ ZipFileManager.java
@@ -0,0 +1,13 @@
+import java.nio.file.Path;
+
+public class ZipFileManager {
+    private Path zipFile;
+
+    public ZipFileManager(Path zipFile) {
+        this.zipFile = zipFile;
+    }
+
+    public void createZip(Path source) throws Exception {
+
+    }
+}
