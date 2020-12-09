c417beb01e52778ef20b1a460e90d2ed1cf9e353
self-harm <lightgraywrite@gmail.com>
2020-12-10 01:30:16 +0300

    Update ZipFileManager.java

--- ZipFileManager.java
+++ ZipFileManager.java
@@ -8,6 +8,20 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception {
+            //create the stream (zip will archive zipFile)
+        try(ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile));
+            InputStream inputStream = Files.newInputStream(source)){
 
+            ZipEntry entry = new ZipEntry(source.getFileName().toString());
+            zipOutputStream.putNextEntry(entry);
+            while (inputStream.available() > 0){
+                //write data from inputStream to zipOutStream
+                zipOutputStream.write(inputStream.read());
+            }
+        }
+        catch (Exception e){
+            e.getStackTrace();
+            System.out.println("Произошла ошибка при архивации файла");
+        }
     }
 }
