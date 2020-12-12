9e69ab5057bcec1a781aa1548cf6ce7fd6ce729e
self-harm <lightgraywrite@gmail.com>
2020-12-12 22:10:19 +0300

    Update ZipCreateCommand.java

--- io.archiver/command/ZipCreateCommand.java
+++ io.archiver/command/ZipCreateCommand.java
@@ -1,4 +1,31 @@
-packege io.archiver.command;
+package io.archive.command;
+
+import io.archive.ConsoleHelper;
+import io.archive.ZipFileManager;
+import io.archive.exception.PathIsNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class ZipCreateCommand extends ZipCommand{
+    @Override
+    public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Создание архива");
+
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("Введите полное имя файла и директирию для архивации:");
+
+            //full path of fileName
+            Path path = Paths.get(ConsoleHelper.readString());
+            zipFileManager.createZip(path);
+
+            ConsoleHelper.writeMessage("Архив создан.");
+        }
+        catch (PathIsNotFoundException e){
+            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+            e.getStackTrace();
+        }
+    }
 }
