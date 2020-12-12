33b823b1f4edb34c6c961fc74aa17027b03fe374
self-harm <lightgraywrite@gmail.com>
2020-12-12 22:11:29 +0300

    Update ZipCommand.java

--- io.archiver/command/ZipCommand.java
+++ io.archiver/command/ZipCommand.java
@@ -1,8 +1,20 @@
 packege io.archiver.command;
 
+import  io.archiver.ConsoleHelper;
+import  io.archiver.ZipFileManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public abstract class ZipCommand implements Command{
     @Override
     public void execute() throws Exception {
 
+    }
+    
+    public ZipFileManager getZipFileManager() throws Exception{
+    String pathString = ConsoleHelper.readString();
+
+    return new ZipFileManager(Paths.get(pathString));
     }
 }
