015bd815ba55c2cce691e73d8e6f3199e1bef9c1
self-harm <lightgraywrite@gmail.com>
2020-12-15 19:32:39 +0300

    Update ZipRemoveCommand.java

--- io.archiver/command/ZipRemoveCommand.java
+++ io.archiver/command/ZipRemoveCommand.java
@@ -1,4 +1,11 @@
 packege io.archiver.command;
 
 public class ZipRemoveCommand extends ZipCommand{
+    @Override
+    public void execute() throws Exception {
+        ZipFileManager zipFileManager = getZipFileManager();
+        ConsoleHelper.writeMessage("Введите путь архива и файл для удаления:");
+
+        zipFileManager.removeFile(Paths.get(ConsoleHelper.readString()));
+    }
 }
