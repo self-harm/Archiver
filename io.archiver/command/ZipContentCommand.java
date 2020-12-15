7c39e5db21352b8fa6d848f4dae28878ed7b6a2a
self-harm <lightgraywrite@gmail.com>
2020-12-15 18:39:47 +0300

    Update ZipContentCommand.java

--- io.archiver/command/ZipContentCommand.java
+++ io.archiver/command/ZipContentCommand.java
@@ -1,4 +1,19 @@
 ppackege io.archiver.command;
 
 public class ZipContentCommand extends ZipCommand{
+    @Override
+    public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Просмотр содержимого архива.");
+
+        ZipFileManager zipFileManager = getZipFileManager();
+
+        ConsoleHelper.writeMessage("Содержимое архива:");
+
+        List<FileProperties> files = zipFileManager.getFilesList();
+        for (FileProperties file : files) {
+            ConsoleHelper.writeMessage(file.toString());
+        }
+
+        ConsoleHelper.writeMessage("Содержимое архива прочитано.");
+    }
 }
