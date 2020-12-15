b60a532997cdf147a3f3555b96471d1281aca1be
self-harm <lightgraywrite@gmail.com>
2020-12-15 18:58:18 +0300

    Update ZipExtractCommand.java

--- io.archiver/command/ZipExtractCommand.java
+++ io.archiver/command/ZipExtractCommand.java
@@ -1,4 +1,21 @@
 packege io.archiver.command;
 
 public class ZipExtractCommand extends ZipCommand{
+  @Override
+    public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Распаковка архива");
+
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("Введите полное имя файла или директории для распаковки:");
+            Path sourcePath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.extractAll(sourcePath);
+
+            ConsoleHelper.writeMessage("Архив распакован.");
+
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+        }
+    }
 }
